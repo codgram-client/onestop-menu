f4a1f82aee6658f1c297e6ccb3ca2ce08991b7cc
Kevork Keheian <kevork.keheian@gmail.com>
2022-12-26 17:41:46 +0200

    app: add facebook open graph meta tags

---
+++ Models/OpenGraph.cs
@@ -0,0 +1,11 @@
+namespace Application.Models;
+
+
+public class OpenGraph
+{
+    public string Url { get; set; }
+    public string Type { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+}
